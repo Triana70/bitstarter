571da9864a9670e7be0744b4c836c1ffbce3dd11
Triana70 <jharor@gmail.com>
2013-07-09 19:54:36 +0000

    wb.js take data from a file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var data = fs.readFileSync("./index.html");
+buf = new Buffer(data);
+var outdata = buf.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(outdata);
 });
 
 var port = process.env.PORT || 5000;
